a6fe2d98bfda6c719e71b07018ece41ba5002301
annettea <annettea@gmail.com>
2024-04-03 13:35:37 -0400

    Testing component.

--- javascripts/discourse/components/custom-homepage-content.js
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -4,11 +4,12 @@ import { service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
 
 export default class CustomHomepageContent extends Component {
-  @service router;
+    @service site;
+    @service router;
 
-  // Checks router for route name. If matches homepage, then return true.
-  get isHomepage() {
-    const { currentRouteName } = this.router;
-    return currentRouteName === `discovery.${defaultHomepage()}`;
-  }
+    // Checks router for route name. If matches homepage, then return true.
+    get isHomepage() {
+        const { currentRouteName } = this.router;
+        return currentRouteName === `discovery.${defaultHomepage()}`;
+    }
 }
