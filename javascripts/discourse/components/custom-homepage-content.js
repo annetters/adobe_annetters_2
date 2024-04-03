216a32d7c5a39b9b6c046e5bbd213a05d0dc02c8
annettea <annettea@gmail.com>
2024-04-03 13:30:09 -0400

    Testing component.

--- javascripts/discourse/components/custom-homepage-content.js
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -1,55 +1,14 @@
 import Component from "@ember/component";
 import { action } from "@ember/object";
-import discourseComputed from "discourse-common/utils/decorators";
+import { service } from "@ember/service";
+import { defaultHomepage } from "discourse/lib/utilities";
 
-export default Component.extend({
-  tagName: "",
-  showMuted: false,
+export default class CustomHomepageContent extends Component {
+  @service router;
 
-  @discourseComputed("showMutedCategories", "filteredCategories.length")
-  mutedToggleIcon(showMutedCategories, filteredCategoriesLength) {
-    if (filteredCategoriesLength === 0) {
-      return;
-    }
-
-    if (showMutedCategories) {
-      return "minus";
-    }
-
-    return "plus";
-  },
-
-  @discourseComputed("showMuted", "filteredCategories.length")
-  showMutedCategories(showMuted, filteredCategoriesLength) {
-    return showMuted || filteredCategoriesLength === 0;
-  },
-
-  @discourseComputed("categories", "categories.length")
-  filteredCategories(categories, categoriesLength) {
-    if (!categories || categoriesLength === 0) {
-      return [];
-    }
-
-    return categories.filter((cat) => !cat.isHidden);
-  },
-
-  @discourseComputed("categories", "categories.length")
-  mutedCategories(categories, categoriesLength) {
-    if (!categories || categoriesLength === 0) {
-      return [];
-    }
-
-    // hide in single category pages
-    if (categories.firstObject.parent_category_id) {
-      return [];
-    }
-
-    return categories.filterBy("hasMuted");
-  },
-
-  @action
-  toggleShowMuted(event) {
-    event?.preventDefault();
-    this.toggleProperty("showMuted");
-  },
-});
+  // Checks router for route name. If matches homepage, then return true.
+  get isHomepage() {
+    const { currentRouteName } = this.router;
+    return currentRouteName === `discovery.${defaultHomepage()}`;
+  }
+}
