a7e87437534c9ed8ef1f2201d386e0fb84cf5c92
annettea <annettea@gmail.com>
2024-04-03 21:15:58 -0400

    Category component added.

--- javascripts/discourse/components/custom-homepage-content.js
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -1,13 +1,15 @@
 import Component from "@glimmer/component";
-import { inject as service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
+import { service } from "@ember/service";
 
 export default class CustomHomepageContent extends Component {
   @service router;
+  @service site;
 
   // Checks router for route name. If matches homepage, then return true.
   get isHomepage() {
     const { currentRouteName } = this.router;
     return currentRouteName === `discovery.${defaultHomepage()}`;
   }
+
 }
